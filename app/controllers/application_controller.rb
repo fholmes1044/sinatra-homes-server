0c0ff636666235ff52b3b43aa590d7746e311090
+ <+>
2023-02-21 10:59:16 -0500

    routing updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,29 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    #{ message: "Getting Homes" }.to_json
- 
-    homes = Home.all
-    homes.to_json
-   
+  get "/hosts" do
+    hosts = Host.all
+    hosts.to_json
   end
 
+  # post '/hosts/:id/homes' do 
+  #   home = home.create(
+  #     body: params[:body],
+  #     home: params[:home]
+  #   )
+  #   home.to_json
+  # end
+  
+  # patch '/hosts/:id/homes' do
+  #   home= Home.find(params[:id])
+  #   home.update(
+  #     body: params[:body]
+  #   )
+  #   home.to_json
+  # end
+
+  # delete "/hosts/:id/homes" do
+  #   home = Home.find(params[:id])
+  #   home.destroy
+  # end
 end
